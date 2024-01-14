f4f7b7f4efc5c494bf463b2fef2bb73bedaa3b27
Natalie <natalie.yeung16@gmail.com>
2024-01-14 16:53:27 -0700

    Create user model

---
+++ api/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+//schema: set of rules and conditions
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true, // must have a username
+      unique: true, // all usernames must be unqiue (aka PRIMARY KEYS)
+    },
+    email: {
+      type: String,
+      required: true, // must have a username
+      unique: true, // all usernames must be unqiue (aka PRIMARY KEYS)
+    },
+    password: {
+      type: String,
+      required: true, // must have a username
+    },
+  },
+  { timestamps: true } // each user will have two timestamps: time of creation and time of edit
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

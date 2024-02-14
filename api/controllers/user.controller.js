d1c66d476d12c03ad7ed90431a5329c79cbd5f68
Natalie <natalie.yeung16@gmail.com>
2024-02-13 19:43:15 -0700

    Create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,40 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     message: "API is working",
   });
+};
+
+//update user
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can update only your account!"));
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profileImage: req.body.profileImage,
+        },
+      },
+      { new: true }
+    ); //need this to view updated user or it will show previous user
+    const { password, ...rest } = updateUser._doc;
+    req.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
